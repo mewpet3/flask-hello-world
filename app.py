79ccfe94164bb96c60f2490a142a9b985fa00db0
mewpet3 <george_gaddhayward@hotmail.co.uk>
2026-02-12 16:13:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
+x = 10
+y = 10
 
 @app.route('/')
 def hello_world():
-    return 'Goodbye World'
+    global x
+    global y
+    
+    return str(x) + " " + str(y)
+
+
+@app.route('/left')
+def hello_world():
+    global x
+    global y
+    x-=1
+    return str(x) + " " + str(y)
+@app.route('/right')
+def hello_world():
+    global x
+    global y
+    x+=1
+    return str(x) + " " + str(y)
+@app.route('/up')
+def hello_world():
+    global x
+    global y
+    y-=1
+    return str(x) + " " + str(y)
+@app.route('/down')
+def hello_world():
+    global x
+    global y
+    y+=1
+    return str(x) + " " + str(y) + '''\n<a href="{{ url_for('/right') }}">Go</a>'''
