28dc42c023b86b6d5de85ba6dda69df67688d63e
mewpet3 <george_gaddhayward@hotmail.co.uk>
2026-02-13 08:47:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,42 @@ app = Flask(__name__)
 x = 10
 y = 10
 
+def baseHTML():
+    return str(x) + " " + str(y) + '''\n<a href="up">up</a>\n<a href="down">down</a>\n<a href="left">left</a>\n<a href="right">right</a>'''
 
 @app.route('/')
 def hello_world():
     global x
     global y
-    return str(x) + " " + str(y)
+    html = baseHTML()
+    return html
 
 
 @app.route('/left')
 def left():
     global x
     global y
+    html = baseHTML()
     x-=1
-    return str(x) + " " + str(y)
+    return html
 @app.route('/right')
 def right():
     global x
     global y
+    html = baseHTML()
     x+=1
-    return str(x) + " " + str(y)
+    return html
 @app.route('/up')
 def up():
     global x
     global y
+    html = ""
     y-=1
-    return str(x) + " " + str(y)
+    return html
 @app.route('/down')
 def down():
     global x
     global y
+    html = baseHTML()
     y+=1
-    return str(x) + " " + str(y) + '''\n<a href="down">down</a>'''
+    return html
